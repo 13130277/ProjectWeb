c203a628bcb24cf52fbc0e89555b2623ff1ab769
LienTam <longnguyen110995@gmail.com>
2016-01-07 11:17:03 +0700

    Tourdaoimp

--- NEWPROJECT/src/DAO/TourDaoImp.java
+++ NEWPROJECT/src/DAO/TourDaoImp.java
@@ -1,10 +1,11 @@
 package DAO;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 
 import DB.ConnectionDB;
 import Model.Tour;
@@ -26,7 +27,6 @@ public class TourDaoImp implements TourDao{
 
 	@Override
 	public ArrayList<Tour> getInfoTour(String sql) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -35,20 +35,25 @@ public class TourDaoImp implements TourDao{
 		ArrayList<Tour> listTour = new ArrayList<>();
 		try {
 		Connection con = ConnectionDB.getConnection();
-			PreparedStatement ps;
-				ps = con.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
+		Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
 			while(rs.next()){
+				System.out.println("abcd");
 				Tour tour = new Tour();
-				tour.setIdTour(rs.getString(1));
-				tour.setTourType(new Tour_Type(rs.getString(10), rs.getString(11), rs.getString(12)));
-				tour.setNameTour(rs.getString(3));
-				tour.setDeparture(rs.getDate(2));
-				tour.setEnd(rs.getDate(5));
-				tour.setAmountDays(rs.getInt(6));
-				tour.setContent(rs.getString(7));
-				tour.setPrice(rs.getDouble(8));
-				tour.setShortContent(rs.getString(9));
+				tour.setIdTour(rs.getString("idTour"));
+				System.out.println(tour.getIdTour());
+				tour.setTourType(new Tour_Type(rs.getString("idTourType"), rs.getString("nameTourType"), rs.getString("parent")));
+				System.out.println(tour.getTourType().toString());
+				tour.setNameTour(rs.getString("nameTour"));
+				System.out.println(tour.getNameTour());
+				tour.setDeparture(new Date(rs.getDate("departure").getTime()));
+				System.out.println(tour.getDeparture());
+				tour.setEnd(new Date(rs.getDate("end").getTime()));
+				System.out.println(tour.getEnd());
+				tour.setAmountDays(rs.getInt("amountDays"));
+				tour.setContent(rs.getString("content"));
+				tour.setPrice(rs.getDouble("price"));
+				tour.setShortContent(rs.getString("shortContent"));
 				listTour.add(tour);
 			}
 			con.close();
